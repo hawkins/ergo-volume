d9ae58a5cbf90ffd1fb115afb1b06ced6f48b027
Josh Hawkins <jkhaccounts@gmail.com>
2017-12-02 17:23:03 -0600

    Refactoring

--- ErgoVolume.py
+++ ErgoVolume.py
@@ -4,7 +4,42 @@ from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
 
 class AudioController(object):
     def __init__(self):
-        self.VOLUME_DELTA = 0.01
+        self.VOLUME_DELTA = 0.1
+        self.super_held = False
+        self.page_down_held = False
+        self.page_up_held = False
+
+    def on_press(self, key):
+        if key is keyboard.Key.cmd:
+            self.super_held = True
+        elif key is keyboard.Key.page_up:
+            self.page_up_held = True
+        elif key is keyboard.Key.page_down:
+            self.page_down_held = True
+
+        if self.super_held:
+            self.handle_volume_adjustment()
+
+    def on_release(self, key):
+        if key is keyboard.Key.cmd:
+            self.super_held = False
+        elif key is keyboard.Key.page_up:
+            self.page_up_held = False
+        elif key is keyboard.Key.page_down:
+            self.page_down_held = False
+
+        # # DEBUG
+        # if key == keyboard.Key.esc:
+        #     # Stop listener
+        #     return False
+
+    def handle_volume_adjustment(self):
+        """Change volume as needed using pycaw."""
+
+        if self.page_up_held:
+            self.increase_volume()
+        if self.page_down_held:
+            self.decrease_volume()
 
     def decrease_volume(self):
         try:
@@ -15,7 +50,6 @@ class AudioController(object):
                 # 0.0 is the min value, reduce by decibels
                 volume = max(0.0, interface.GetMasterVolume() - self.VOLUME_DELTA)
                 interface.SetMasterVolume(volume, None)
-                print('Volume reduced to', volume)  # debug
         except OSError as e:
             print(e)
 
@@ -28,71 +62,20 @@ class AudioController(object):
                 # 1.0 is the max value, raise by decibels
                 volume = min(1.0, interface.GetMasterVolume() + self.VOLUME_DELTA)
                 interface.SetMasterVolume(volume, None)
-                print('Volume raised to', volume)  # debug
         except OSError as e:
             print(e)
 
 
-# These variables describe the state of the application
-super_held = False
-page_up_held = False
-page_down_held = False
-audio_controller = AudioController()
 
-
-def handle_volume_adjustment():
-    """Change volume as needed using pycaw."""
-
-    global page_down_held
-    global page_up_held
-    global audio_controller
-
-    if page_up_held:
-        audio_controller.increase_volume()
-    if page_down_held:
-        audio_controller.decrease_volume()
-
-
-def on_press(key):
-    global super_held
-    global page_down_held
-    global page_up_held
-
-    if key is keyboard.Key.cmd:
-        super_held = True
-    elif key is keyboard.Key.page_up:
-        page_up_held = True
-    elif key is keyboard.Key.page_down:
-        page_down_held = True
-
-    if super_held:
-        handle_volume_adjustment()
-
-def on_release(key):
-    global super_held
-    global page_down_held
-    global page_up_held
-
-    if key is keyboard.Key.cmd:
-        super_held = False
-    elif key is keyboard.Key.page_up:
-        page_up_held = False
-    elif key is keyboard.Key.page_down:
-        page_down_held = False
-
-    # DEBUG
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
-
+# For some unknown reason, this must be called once before enterring
+# listener.join() to prevent a CoInitialize related OSError
 sessions = AudioUtilities.GetAllSessions()
-# for session in sessions:
-#     session._ctl.QueryInterface(ISimpleAudioVolume)
 
+# The audio controller will manage program volume levels
+audio_controller = AudioController()
 
 # Collect events until released
 with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
+        on_press=audio_controller.on_press,
+        on_release=audio_controller.on_release) as listener:
     listener.join()
